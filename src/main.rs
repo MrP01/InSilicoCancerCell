b5d553cbe20ea5864cef5f673108c6ab41aa8955
MrP01 <peter@waldert.at>
2024-03-19 21:55:11 +0100

    extract parts of main() into utils

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 use cell::A549CancerCell;
 use patchclampdata::{CellPhase, PatchClampData, PatchClampProtocol};
 use pulseprotocol::{ProtocolGenerator, PulseProtocol};
+use utils::evaluate_match;
 
 mod cell;
 mod channels;
@@ -12,19 +13,10 @@ mod pulseprotocol;
 mod utils;
 
 fn main() {
-  simplelog::CombinedLogger::init(vec![simplelog::TermLogger::new(
-    simplelog::LevelFilter::Info,
-    simplelog::Config::default(),
-    simplelog::TerminalMode::Mixed,
-    simplelog::ColorChoice::Auto,
-  )])
-  .unwrap();
-
+  utils::setup_logging();
   let measurements = PatchClampData::load(PatchClampProtocol::Ramp, CellPhase::G0).unwrap();
   let pulse_protocol = PulseProtocol::default();
   let mut cell = A549CancerCell::new();
   let simulation = cell.simulate(pulse_protocol);
-  let rows = measurements.current.len();
-  let error = (simulation.as_dvec().rows_range(0..rows) - measurements.current).norm_squared();
-  log::info!("Simulation match with measurements: {:.3}", error);
+  evaluate_match(measurements, simulation);
 }

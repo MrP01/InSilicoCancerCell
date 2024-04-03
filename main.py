41264d6cd5f2c5125e7af74ba64bf8859db53bbd
MrP01 <peter@waldert.at>
2024-04-03 16:57:26 +0200

    update CI workflow, imports and add .plot submodule

---
+++ main.py
@@ -0,0 +1,11 @@
+import pathlib
+
+import in_silico_cancer_cell as insilico
+import in_silico_cancer_cell.plot as insilico_plot
+
+RESULTS = pathlib.Path(__file__).resolve().parent / "figures" / "results"
+
+
+if __name__ == "__main__":
+    insilico_plot.set_results_folder(RESULTS)
+    insilico_plot.plot_measurement()
--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,9 @@ mod patchclampdata;
 mod pulseprotocol;
 mod utils;
 
-use pyo3::prelude::*;
-
 /// A Python module implemented in Rust.
-#[pymodule]
-fn _in_rusty_silico(_py: Python, m: &PyModule) -> PyResult<()> {
+#[pyo3::pymodule]
+fn _in_rusty_silico(_py: pyo3::Python, m: &pyo3::types::PyModule) -> pyo3::PyResult<()> {
   m.add_class::<patchclampdata::PatchClampProtocol>()?;
   m.add_class::<patchclampdata::CellPhase>()?;
   m.add_class::<cell::A549CancerCell>()?;

fa010e2c16fe7083ae0d6891dc56808e09257d13
MrP01 <peter@waldert.at>
2024-04-04 17:19:49 +0200

    initialize Astro frontend / dashboard with WASM integration

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,30 @@ mod patchclampdata;
 mod pulseprotocol;
 mod utils;
 
+use wasm_bindgen::prelude::*;
+use web_sys::console;
+
+// If you don't want to use `wee_alloc`, you can safely delete this.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub fn run() -> Vec<f64> {
+  return vec![2.0, 3.45, 11.0];
+}
+
+// This is like the `main` function, except for JavaScript.
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue> {
+  // This provides better error messages in debug mode.
+  // It's disabled in release mode so it doesn't bloat up the file size.
+  #[cfg(debug_assertions)]
+  console_error_panic_hook::set_once();
+  console::log_1(&JsValue::from_str("Hello world!"));
+  Ok(())
+}
+
 /// A Python module implemented in Rust.
 #[pyo3::pymodule]
 fn _in_rusty_silico(_py: pyo3::Python, m: &pyo3::types::PyModule) -> pyo3::PyResult<()> {

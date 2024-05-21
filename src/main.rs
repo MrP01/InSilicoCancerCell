6cdc9dd753349442406c9758403a6e4a7f97b399
MrP01 <peter@waldert.at>
2024-05-21 17:03:31 +0200

    add clap for small command line interface

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,11 @@ mod utils;
 use cell::evaluate_match;
 use cell::A549CancerCell;
 use cell::TotalCurrentRecord;
+use clap::{Parser, Subcommand};
 use patchclampdata::{CellPhase, PatchClampData, PatchClampProtocol};
 use pulseprotocol::DefaultPulseProtocol;
 
-fn evaluate_on_langthaler_et_al(measurements: PatchClampData) {
+fn evaluate_on_langthaler_et_al_counts(measurements: PatchClampData) {
   let pulse_protocol = DefaultPulseProtocol {};
   let mut cell = A549CancerCell::new();
   cell.set_langthaler_et_al_channel_counts(measurements.phase.clone());
@@ -24,8 +25,39 @@ fn evaluate_on_langthaler_et_al(measurements: PatchClampData) {
   evaluate_match(&measurements, recorded);
 }
 
+#[derive(Parser)]
+#[clap(arg_required_else_help = true)]
+#[command(
+  about = "In-Silico Cancer Cell Model Simulator",
+  author = "Peter Waldert <peter@waldert.at>",
+  version = "0.1.0"
+)]
+struct Cli {
+  /// Turn debugging information on
+  #[arg(short, long, action = clap::ArgAction::Count)]
+  debug: u8,
+
+  #[command(subcommand)]
+  command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+  RunSingle,
+  Optimise,
+}
+
 fn main() {
   utils::setup_logging();
   let measurements = PatchClampData::load(PatchClampProtocol::Ramp, CellPhase::G0).unwrap();
-  optimisation::find_best_fit_for(measurements);
+
+  let cli = Cli::parse();
+  match cli.command {
+    Command::RunSingle => {
+      evaluate_on_langthaler_et_al_counts(measurements);
+    }
+    Command::Optimise => {
+      optimisation::find_best_fit_for(measurements);
+    }
+  }
 }
